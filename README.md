d5ff4dc3f1c4d0d9ea6aaa1c75d9bc64933a4e99
lucas <lucas.tx.reis@gmail.com>
2019-12-26 12:01:17 -0300

    Adding README and fixing helloWorld

---
+++ README.md
@@ -0,0 +1,9 @@
+# Parallel tinkering
+
+Experiments on Parallelism for C/C++. In each program in this repo I'll try to explore some of the omp.h functionalities. 
+Aiming to build a future "quick reference", there is a structure in every .cpp file you see here. Currently, it consists of:
+
+. Each .cpp file header will contain my observations on the program's behavior
+. They will be verbose, not only for future reference but to assure I understand what I'm doing on every step
+. So far, everything will be done using the omp.h library, meaning you have to compile with the flag "-fopenmp"
+. No -O3 will be used for testing, since the purpose is to study speedups from parallelism and not other optimization techniques
--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,3 +1,8 @@
+/* In this example you can see that there is not particular
+order of threads for execution - race condition. So the "# of threads" 
+print can occur anywhere and you can see the "Hello parallel... thread #" 
+with random thread identifications from (0,1,...,nThreads) */
+
 #include <cstdio>
 #include <omp.h>
 
@@ -7,16 +12,20 @@ int main() {
 	int master = 0;
 
 	// Spawning threads, each one with its own @tId and @nThreads
+	// which is possible due to the private(var1, var2, ..., varN) function
 	#pragma omp parallel private(tId, nThreads)
 	{
 	
 	tId = omp_get_thread_num();
 
-	printf("Hello parallel world, from thread n# %d\n", tId);
-	
-	if(tId == master) printf("# of threads %d\n", nThreads);
+	printf("Hello parallel world, from thread #%d\n", tId);
+
+	// Assuming master threads to be 0
+	if(tId == master) {
+		nThreads = omp_get_num_threads();
+		printf("# of threads %d\n", nThreads);
+	}
 	
 	} // Reunite threads
 
-	
 }
